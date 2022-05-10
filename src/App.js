3ccf89ab57416400481a4b2d3369e4d2d4450d48
trc96 <toddcrandall1996@gmail.com>
2022-05-10 15:35:17 -0600

    Added a form with props and listeners. Passed functions from children to parent.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import Expenses from "./components/Expenses/Expenses"
 import React from 'react';
+
+import Expenses from "./components/Expenses/Expenses"
+import NewExpense from './components/NewExpense/NewExpense';
 //Line 2 is for older react code, needed in the past for the commented out return format, It's in every file that has JSX
 
 const App = (props) => {
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  }
 
   // return React.createElement(
   //   'div', 
@@ -10,9 +17,10 @@ const App = (props) => {
   //   React.createElement('h2', {}, `Let's get started!`), 
   //   React.createElement(Expenses, {items: props.expenseItem})
   //   );
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={props.ExpenseItem}/>
     </div>
   );
